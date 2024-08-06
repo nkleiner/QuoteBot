30660a89a4f8cf65a00e982c99ad71acb803ad86
Ethan Marsh <97853540+LordMuffinBoi@users.noreply.github.com>
2024-08-06 18:58:02 -0400

    Worked smarter not harder
    Utilized TextEntryDialog from wxpython, instead of trying to create a whole Text Entry field from scratch

--- src/GUI/QuoteBotGUI.py
+++ src/GUI/QuoteBotGUI.py
@@ -4,36 +4,94 @@ from tkinter.filedialog import askopenfilename
 from tkinter import filedialog
 import os
 
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='QuoteBot Setup')
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        my_btn = wx.Button(panel, label='Save Configuration')
-        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
-        panel.SetSizer(my_sizer)        
-        self.Show()
 
-    def on_press(self, event):
-        value = self.text_ctrl.GetValue()
-        if not value:
-            print("You didn't enter anything!")
-        else:
-            print(f'You typed: "{value}"')
+def ask(parent=None, message='', default_value=''):
+    dlg = wx.TextEntryDialog(parent, message, value=default_value)
+    dlg.ShowModal()
+    result = dlg.GetValue()
+    dlg.Destroy()
+    return result
 
-            dirPath = filedialog.askdirectory()
-            fileName = "quoteBot_config.json"
-            file_path = os.path.join(dirPath,fileName)
-            print(file_path)
-            with open(file_path, "w") as file:
-                file.write(f'userText= {value}')
-                file.close()
+# Initialize wx App
+app = wx.App()
+app.MainLoop()
+
+# Call Dialog
+token_value = ask(message = 'Enter Discord token:')
+print ('Your token is', token_value)
 
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+with open("config.json", "w") as file:
+    file.write('{\n\t"token":"'+token_value+'"')
+    file.close()
+
+channelID = ask(message = 'Enter channel ID:')
+print ("channel ID is: " + channelID)
+
+
+with open("config.json", "a") as file:
+    file.write('\n\t"quoteChannelID":"'+channelID+'"\n}')
+    file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##class MyFrame(wx.Frame):    
+##    def __init__(self):
+##        super().__init__(parent=None, title='QuoteBot Setup')
+##        panel = wx.Panel(self)        
+##        my_sizer = wx.BoxSizer(wx.VERTICAL)        
+##        self.text_ctrl = wx.TextCtrl(panel)
+##        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+##        my_btn = wx.Button(panel, label='Save Configuration')
+##        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+##        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+##        panel.SetSizer(my_sizer)        
+##        self.Show()
+##
+##    def on_press(self, event):
+##        value = self.text_ctrl.GetValue()
+##        if not value:
+##            print("You didn't enter anything!")
+##        else:
+##            print(f'You typed: "{value}"')
+##
+##            dirPath = filedialog.askdirectory()
+##            fileName = "quoteBot_config.json"
+##            file_path = os.path.join(dirPath,fileName)
+##            print(file_path)
+##            with open(file_path, "w") as file:
+##                file.write(f'"token":"{value}"')
+##                file.close()
+##
+##
+##if __name__ == '__main__':
+##    app = wx.App()
+##    frame = MyFrame()
+##    app.MainLoop()
